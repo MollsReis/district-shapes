306720f07b66030ff4748436d65ad148ca334a45
EvilScott <reis.robert.s@gmail.com>
2022-07-12 11:31:47 -0700

    update/fix db tables

--- initdb.sh
+++ initdb.sh
@@ -7,16 +7,19 @@ psql <<-EOSQL
     state_name TEXT NOT NULL,
     gnis_id TEXT NOT NULL
   );
-  COPY states FROM '/app/state.txt' WITH (FORMAT CSV, DELIMITER '|', HEADER TRUE)";
-  CREATE VIEW v_districts AS
-  SELECT gid      AS id,
-         abbr     AS state_abbr,
-         namelsad AS district_name,
-         aland    AS land_area,
-         awater   AS water_area,
-         intptlat AS lat,
-         intptlon AS lon,
-         geom
+  COPY states FROM '/app/state.txt' WITH (FORMAT CSV, DELIMITER '|', HEADER TRUE);
+  CREATE MATERIALIZED VIEW mv_districts AS
+  SELECT gid                                                            AS id,
+         abbr                                                           AS state_abbr,
+         namelsad                                                       AS district_name,
+         aland                                                          AS land_area,
+         awater                                                         AS water_area,
+         intptlat                                                       AS lat,
+         intptlon                                                       AS lon,
+         geom,
+         ST_AsGeoJSON(geom)::JSONB                                AS geojson,
+         ST_Area(geom) / ST_Perimeter(geom)                 AS compactness_ratio,
+         1 - (ST_Area(geom) / ST_Area(ST_ConvexHull(geom))) AS shape_complexity_index
   FROM tl_2021_us_cd116 t
   JOIN states s ON (s.state_fp = t.statefp);
 EOSQL
